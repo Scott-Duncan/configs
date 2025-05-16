c4dfc8f5b9d1efe122882dbc4cad49216676077d
Scott Duncan <scott@kisuitech.com>
2025-05-16 10:06:08 +0900

    Updates to polybar

--- polybar/single_launch.sh
+++ polybar/single_launch.sh
@@ -1,7 +1,8 @@
-#! /bin/sh 
+#! /bin/sh
 
 killall -q polybar
 
-
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-polybar main-screen &  polybar second-screen & 
+polybar main-screen &
+polybar second-screen &
+polybar laptop-screen &
